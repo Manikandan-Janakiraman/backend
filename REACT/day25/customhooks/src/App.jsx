efcac8ead9442cbb19565bd96f12f6107d625c5d
unknown <manij472@gmail.com>
2026-02-03 22:12:07 +0530

    day25

---
+++ REACT/day25/customhooks/src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Hooks from './Hooks'
+import FetchTask from './FetchTask'
+import ProductFilter from './ProductFilter'
+import AdvancedHooks from './AdvancedHooks'
+
+const App = () => {
+  return (
+    <>
+    <Hooks />
+    <FetchTask />
+    <ProductFilter />
+    <AdvancedHooks />
+    
+    </>
+  )
+}
+
+export default App
