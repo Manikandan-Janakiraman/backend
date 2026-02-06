55efeadd463e7005c3fbbf362ec855a81d614577
unknown <manij472@gmail.com>
2026-02-06 21:49:41 +0530

    day27

---
+++ REACT/day27/usecallbackprocess/src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+import MemoProcess from './MemoProcess'
+import ProcessCallBack from './ProcessCallBack'
+
+const App = () => {
+  return (
+    <>
+    <MemoProcess />
+    
+    
+    
+    <ProcessCallBack />
+    </>
+  )
+}
+
+export default App
