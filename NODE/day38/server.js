acfe87a82f493e9cb7790bd668f3376adde3c2ad
unknown <manij472@gmail.com>
2026-02-24 23:57:19 +0530

    day38

---
+++ NODE/day38/server.js
@@ -0,0 +1,30 @@
+import express from 'express'
+import dotenv from 'dotenv'
+import cors from 'cors'
+import route from './routes/route.js'
+
+
+import connectdb from './config/db.js'
+
+
+dotenv.config()
+connectdb()
+
+const deptapp = express()
+
+deptapp.use(cors())
+
+deptapp.use(express.json())
+
+deptapp.use("/api/dept",route)
+
+const PORT =process.env.PORT || 3000
+
+deptapp.listen(PORT,()=>{
+
+console.log(`server connected successfully ${PORT}`);
+
+
+
+})
+// http://localhost:5000/api/dept
